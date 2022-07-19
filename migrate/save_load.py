c3081e153f42f012346856075fca04ee81246e05
RedTTG <redtonehair@gmail.com>
2022-07-19 05:44:24 +0300

    get em out of here

--- migrate/save_load.py
+++ migrate/save_load.py
@@ -3,7 +3,6 @@ import pickle, os
 def save(file,*args):
     with open(file, 'wb') as f: pickle.dump(args, f)
 def load(file):
-    args = []
     if os.path.exists(file):
         with open(file,"rb") as f: args = pickle.load(f)
         return args
